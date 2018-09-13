888b2be777fb46c38438611a5aead2649968892a
DESKTOP-V2HTA2D\masaty <gogogagm3@gmail.com>
2018-09-14 02:16:16 +0900

    <初コミ>

---
+++ SwordMan/SwordMan.NativeActivity/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "DxLib.h"
+#include "GameController/GameMain.hpp"
+
+#ifdef __ANDROID__
+// Android版のコンパイルだったら android_main
+int android_main(void)
+#else
+// Windows版のコンパイルだったら WinMain
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#endif
+{
+	GameMain game;
+	game.Run();
+	return 0;                   // ソフトの終了 
+}
