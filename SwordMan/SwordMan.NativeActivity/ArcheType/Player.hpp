d4db28b56f67838cff0f89616cd6b678edee1abb
DESKTOP-V2HTA2D\masaty <gogogagm3@gmail.com>
2018-09-17 01:40:29 +0900

    <プレイヤーのジャンプ処理>

--- SwordMan/SwordMan.NativeActivity/ArcheType/Player.hpp
+++ SwordMan/SwordMan.NativeActivity/ArcheType/Player.hpp
@@ -1,1 +1,35 @@
-﻿
+﻿//----------------------------------------------------
+//!@file	Player.hpp
+//!@brief	プレイヤーの原型を作成します
+//!@author	日比野　真聖
+//!@date	2018/9/14
+//----------------------------------------------------
+#pragma once
+#include "../ECS/ECS.hpp"
+#include "../../Components/Collider.hpp"
+#include "../../Components/Renderer.hpp"
+#include "../GameController/Game.h"
+#include "../Components/Jump.hpp"
+#include "../Components/Think.hpp"
+#include "../Components/Behavior.hpp"
+
+namespace ECS
+{
+	class PlayerArcheType : public IArcheType<const Vec2&,const Vec2&>
+	{
+	public:
+		ECS::Entity* operator()(const Vec2& pos,const Vec2& size)
+		{
+			ECS::Entity*	entity = &ECS::EcsSystem::GetManager().AddEntity();
+			entity->AddComponent<Position>(pos);
+			entity->AddComponent<Rotation>();
+			entity->AddComponent<JumpMove>();
+			entity->AddComponent<Think>();
+			entity->AddComponent<Behavior>();
+			entity->AddComponent<HitBase>(static_cast<float>(size.x), static_cast<float>(size.y)).SetColor(255,0,0);
+			entity->AddComponent<ECS::SimpleDraw>("a");
+			entity->AddGroup(ENTITY_GROUP::Player);
+			return entity;
+		}
+	};
+}
