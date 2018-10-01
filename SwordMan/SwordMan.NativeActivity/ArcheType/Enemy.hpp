3dc798883effc79424c638f40733b00409978597
DESKTOP-V2HTA2D\masaty <gogogagm3@gmail.com>
2018-10-02 02:13:48 +0900

    敵のアーキタイプの作成

--- SwordMan/SwordMan.NativeActivity/ArcheType/Enemy.hpp
+++ SwordMan/SwordMan.NativeActivity/ArcheType/Enemy.hpp
@@ -15,120 +15,43 @@
 #include "../Components/AnimationController.hpp"
 #include "../Components/EnemyDefaultMove.hpp"
 #include "../Components/ComponentDatas/EnemyData.hpp"
-
+#include "../Components/EnemyJumpMove.hpp"
 
 
 namespace ECS
 {
-	//----------------------------------
-	//敵生成器
-	//----------------------------------
-	class EnemyCreator
+	class EnemyCommonArcheType : public IArcheType<const EnemyCommonData&,const int>
 	{
 	public:
-		//敵の生成
-		//型1: 渡すデータの型
-		//型2: 生成するアーキタイプの型
-		//引数: 渡すデータ型のデータ
-		template<typename DataType, typename FuncType>
-		static void Create(const DataType& data)
+		ECS::Entity* operator()(const EnemyCommonData& data,const int id)
 		{
-			FuncType()(data);
-		}
-	};
-
-	//共通
-	class EnemyCommonArcheType : public IArcheType<const EnemyCommonData&>
-	{
-	public:
-		ECS::Entity* operator()(const EnemyCommonData& data)
-		{
-			ECS::Entity*	entity = &ECS::EcsSystem::GetManager().AddEntity();
+			ECS::Entity* entity = &ECS::EcsSystem::GetManager().AddEntity();
 			entity->AddComponent<Position>(data.pos);
-			entity->AddComponent<Velocity>();
+			entity->AddComponent<Velocity>().val.x = data.moveSpeed;
+			entity->AddComponent<HitBase>(data.size.x,data.size.y);
+			entity->AddComponent<EnemyDefaultMove>();
 			entity->AddComponent<Direction>();
 			entity->AddComponent<AnimationDraw>(data.imageName.c_str());
 			entity->AddComponent<AnimationController>(data.changeAnimFrameTime, data.animNum);
 			entity->AddGroup(ENTITY_GROUP::Enemy);
-			return entity;
-		}
-	};
 
-	//ノーマル(青鎧・水色羽)
-	class NormalEnemyArcheType : public IArcheType<const NormalEnemyData&>
-	{
-	public:
-		ECS::Entity* operator()(const NormalEnemyData& data)
-		{
-			ECS::Entity* entity = EnemyCommonArcheType()(data.commonData);
-			AddCollision(entity, data);
-			entity->AddComponent<ECS::EnemyDefaultMove>();
+			switch (id)
+			{
+			case 0:	//青鎧
+				break;
+			case 1: //赤
+				break;
+			case 2: //緑バネ
+				entity->AddComponent<Physics>();
+				entity->AddComponent<TriggerJumpMove>(data.jumpPower);
+				entity->AddComponent<EnemyJumpMove>(data.changeAnimFrameTime * 2);
+				break;
+			case 3: //水色羽
+				break;
+			case 4: //ゴール
+				break;
+			}
 			return entity;
-		}
-	private:
-		void AddCollision(ECS::Entity* entity, const NormalEnemyData& data)
-		{
-			if (!data.collisionData.commonData.isCollision) { return; }
-			entity->AddComponent<HitBase>(data.collisionData.boxSize.x,data.collisionData.boxSize.y);
-		}
-	};
-
-	//赤車輪
-	class LeftMoveEnemyArcheType : public IArcheType<const LeftMoveEnemyData&>
-	{
-	public:
-		ECS::Entity* operator()(const LeftMoveEnemyData& data)
-		{
-			ECS::Entity* entity = EnemyCommonArcheType()(data.commonData);
-			AddCollision(entity, data);
-			entity->AddComponent<EnemyDefaultMove>();
-			return entity;
-		}
-	private:
-		void AddCollision(ECS::Entity* entity, const LeftMoveEnemyData& data)
-		{
-			if (!data.collisionData.commonData.isCollision) { return; }
-			entity->AddComponent<HitBase>(data.collisionData.boxSize.x, data.collisionData.boxSize.y);
-		}
-	};
-
-	//緑バネ
-	class JumpMoveEnemyArcheType : public IArcheType<const JumpMoveEnemyData&>
-	{
-	public:
-		ECS::Entity* operator()(const JumpMoveEnemyData& data)
-		{
-			ECS::Entity* entity = EnemyCommonArcheType()(data.commonData);
-			AddCollision(entity, data);
-			entity->AddComponent<EnemyDefaultMove>();
-			//entity->AddComponent<Physics>();
-			//entity->AddComponent<TriggerJumpMove>(data.jumpPow);
-			return entity;
-		}
-	private:
-		void AddCollision(ECS::Entity* entity, const JumpMoveEnemyData& data)
-		{
-			if (!data.collisionData.commonData.isCollision) { return; }
-			entity->AddComponent<HitBase>(data.collisionData.boxSize.x, data.collisionData.boxSize.y);
-		}
-	};
-
-	//ゴール
-	class GoalArcheType : public IArcheType<const GoalData&>
-	{
-	public:
-		ECS::Entity* operator()(const GoalData& data)
-		{
-			ECS::Entity* entity = EnemyCommonArcheType()(data.commonData);
-			AddCollision(entity, data);
-			entity->AddComponent<EnemyDefaultMove>();
-			return entity;
-		}
-	private:
-		void AddCollision(ECS::Entity* entity, const GoalData& data)
-		{
-			if (!data.collisionData.commonData.isCollision) { return; }
-			entity->AddComponent<HitBase>(data.collisionData.boxSize.x, data.collisionData.boxSize.y);
 		}
 	};
 }
