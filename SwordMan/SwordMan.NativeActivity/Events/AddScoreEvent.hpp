3746c5e437a12ebf89d2b96c463933472fb89694
DESKTOP-V2HTA2D\masaty <gogogagm3@gmail.com>
2018-10-08 02:05:24 +0900

    スコアの計算を修正ver2

--- SwordMan/SwordMan.NativeActivity/Events/AddScoreEvent.hpp
+++ SwordMan/SwordMan.NativeActivity/Events/AddScoreEvent.hpp
@@ -19,76 +19,79 @@ namespace Event
 	public:
 		static void Do()
 		{
-				std::vector<ECS::Entity*> addScores;
-				ECS::Entity* totalScore;
-				const auto& gameUI = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::GameUI);
-				for (const auto& ui : gameUI)
+			std::vector<ECS::Entity*> addScores;
+			ECS::Entity* totalScore;
+			const auto& gameUI = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::GameUI);
+			for (const auto& ui : gameUI)
+			{
+				if (ui->HasComponent<ECS::AddScoreTag>())
 				{
-					if (ui->HasComponent<ECS::AddScoreTag>())
+					addScores.push_back(ui);
+				}
+				if (ui->HasComponent<ECS::TotalScoreDraw>())
+				{
+					totalScore = ui;
+				}
+			}
+			const auto& attackCollisions = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Wepon);
+			const auto& enemys = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Enemy);
+			for (const auto& collision : attackCollisions)
+			{
+				for (const auto& enemy : enemys)
+				{
+					if (Collision::BoxAndBox<ECS::HitBase, ECS::HitBase>(*collision, *enemy))
 					{
-						addScores.push_back(ui);
-					}
-					if (ui->HasComponent<ECS::TotalScoreDraw>())
-					{
-						totalScore = ui;
+						const auto& players = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Player);
+						if (players.size() == 0) { return; }
+						Vec2 playerPos = players[0]->GetComponent<ECS::Position>().val;
+
+						Vec2 enemyPos = enemy->GetComponent<ECS::Position>().val;
+						Vec2 enemySize = Vec2(enemy->GetComponent<ECS::HitBase>().w() / 2.0f, enemy->GetComponent<ECS::HitBase>().h() / 2.0f);
+						float enemyLength = fabsf(enemySize.Length());
+
+						//コリジョンの幅と高さ
+						Vec2 collisionPos = collision->GetComponent<ECS::Position>().val;
+						collisionPos.x += 96.0f;
+						Vec2 collisionSize = Vec2(collision->GetComponent<ECS::HitBase>().w() / 2.0f, collision->GetComponent<ECS::HitBase>().h() / 2.0f);
+						float collisionLength = fabsf(collisionSize.Length());
+
+						//プレイヤーと敵の距離
+						Vec2 distance = enemyPos - collisionPos;
+						float length = fabsf(distance.Length());
+						float scoreLength = collisionLength + enemyLength;
+
+						int plusScore = CalcScore(length,scoreLength);
+						//追加スコアとスコアの番号を検出
+						int addScoreNum = static_cast<int>(addScores.size());
+						//スコアEntityを作成
+						ECS::AddScoreArcheType()("font", Vec2(0, 50 + (addScoreNum % 3) * 32), plusScore);
+						totalScore->GetComponent<ECS::TotalScoreDraw>().AddScore(plusScore);
+						break;
 					}
 				}
-				const auto& attackCollisions = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Wepon);
-				const auto& enemys = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Enemy);
-				for (const auto& collision : attackCollisions)
+			}
+		}
+		private:
+			static int CalcScore(const float distance, const float scoreLength)
+			{
+				int plusScore = 0;
+				if (distance >= scoreLength * 0.8f)
 				{
-					for (const auto& enemy : enemys)
-					{
-						if (Collision::BoxAndBox<ECS::HitBase, ECS::HitBase>(*collision, *enemy))
-						{
-							const auto& players = ECS::EcsSystem::GetManager().GetEntitiesByGroup(ENTITY_GROUP::Player);
-							if (players.size() == 0) { return; }
-							for (const auto& player : players)
-							{
-								//プレイヤーと敵の距離を検知します
-								Vec2 playerPos = player->GetComponent<ECS::Position>().val;
-								Vec2 collisionLength = Vec2(collision->GetComponent<ECS::HitBase>().w(), collision->GetComponent<ECS::HitBase>().h());
-								Vec2 collisionPos = Vec2(collision->GetComponent<ECS::HitBase>().x(), collision->GetComponent<ECS::HitBase>().y());
-								Vec2 enemyPos = enemy->GetComponent<ECS::Position>().val;
-
-								//プレイヤーと敵の距離
-								Vec2 distance = enemyPos - playerPos;
-								float length = fabsf(distance.Length());
-								//最小距離と最大距離の検出
-								distance = (collisionPos + (collisionLength.x / 2.0f)) - playerPos;
-								float minLength = fabsf(distance.Length());
-								distance = (collisionPos + collisionLength.x) - playerPos;
-								float maxLength = fabsf(distance.Length());
-
-								//追加スコアとスコアの番号を検出
-								int plusScore = DecideScore(length, minLength, maxLength);
-								int addScoreNum = static_cast<int>(addScores.size());
-								//スコアEntityを作成
-								ECS::AddScoreArcheType()("font", Vec2(0,50 + (addScoreNum%3) * 32), plusScore);
-								totalScore->GetComponent<ECS::TotalScoreDraw>().AddScore(plusScore);
-								break;
-							}
-						}
-					}
+					plusScore = 50;
+				}
+				else if (distance >= scoreLength * 0.6f)
+				{
+					plusScore = 100;
+				}
+				else if (distance >= scoreLength * 0.3f)
+				{
+					plusScore = 150;
+				}
+				else if (distance >= 0)
+				{
+					plusScore = 200;
+				}
+				return plusScore;
 			}
-		}
-	private:
-		//距離から得点の値を決める
-		static int	DecideScore(const float length, const float minLength, const float maxLength)
-		{
-			if (length <= minLength)
-			{
-				return 200;
-			}
-			if (length >= maxLength - 20)
-			{
-				return 50;
-			}
-			if (minLength < length && length < maxLength - 30)
-			{
-				return 150;
-			}
-			return 100;
-		}
 	};
 }
