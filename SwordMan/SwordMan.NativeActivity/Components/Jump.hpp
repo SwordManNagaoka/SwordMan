a2e93aca0e2c5b6e24158cb4f31a168c6e5386cc
Neighbor Sound <rsp44316@gmail.com>
2018-10-15 09:35:31 +0900

    Update Jump.hpp

--- SwordMan/SwordMan.NativeActivity/Components/Jump.hpp
+++ SwordMan/SwordMan.NativeActivity/Components/Jump.hpp
@@ -1,4 +1,9 @@
-﻿#pragma once
+/**
+* @file Jump.hpp
+* @brief Jumpのアルゴリズムをまとめたファイルです
+* @author tonarinohito
+*/﻿
+#pragma once
 #include "../ECS/ECS.hpp"
 #include "../Utility/Vec.hpp"
 #include "../ResourceManager/ResourceManager.hpp"
